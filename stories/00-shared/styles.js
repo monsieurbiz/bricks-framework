e053d15d1ad1e9a1d3e76c5530bfdd96bbb288e4
Guirec Lefort <g.lefort@monsieurbiz.com>
2020-10-28 02:01:51 +0100

    Documents the default colors

---
+++ stories/00-shared/styles.js
@@ -0,0 +1,37 @@
+export const color = {
+  bg: 'hsl(0, 0%, 100%)',
+
+  // Contrast
+  contrast: {
+    lower: 'hsl(0, 0%, 95%)',
+    low: 'hsl(0, 0%, 80%)',
+    medium: 'hsl(0, 0%, 40%)',
+    high: 'hsl(0, 0%, 20%)',
+    higher: 'hsl(0, 0%, 10%)',
+  },
+
+  // Palette
+  primary: {
+    base: 'hsl(250, 68%, 58%)',
+    light: 'hsl(250, 68%, 68%)',
+    dark: 'hsl(250, 68%, 48%)',
+  },
+
+  // Status
+  status: {
+    info: 'hsl(219, 100%, 65%)',
+    success: 'hsl(116, 54%, 42%)',
+    warning: 'hsl(34, 100%, 50%)',
+    error: 'hsl(7, 70%, 50%)',
+  },
+
+  // Networks
+  networks: {
+    facebook: 'hsl(221, 44%, 41%)',
+    instagram: 'hsl(207, 34%, 48%)',
+    twitter: 'hsl(196, 100%, 46%)',
+    linkedin: 'hsl(358, 79%, 45%)',
+    pinterest: 'hsl(199, 100%, 36%)',
+    youtube: 'hsl(358, 73%, 46%)',
+  },
+}
